ffbc47d14c79631b9b5f7c7e488dc1116a293064
DaveyDaVinci <DaveyDaVinci@gmail.com>
2012-08-01 03:48:07 -0400

    Updated with proper gold and bronze apps

--- goldApp/js/main.js
+++ goldApp/js/main.js
@@ -6,66 +6,20 @@ MiU 0612
 Project 3
 */
 //test
-<<<<<<< HEAD
-=======
 
 
->>>>>>> origin/gh-pages
 var parseMateData = function(data){
 	console.log(data);	
 };
 
-<<<<<<< HEAD
-$(document).bind('pageinit', function(){
-
-	var mateData = $('#addMateForm');
-	
-=======
 
 $(document).bind('pageinit', function(){
 
 	var mateData = $('#addMateForm');
->>>>>>> origin/gh-pages
 
 	mateData.validate({
 		invalidHandler: function(form, validator){},
 		submitHandler: function(){
-<<<<<<< HEAD
-			var data = mateData.serializeArray();
-			parseMateData(data);
-		}
-	});
-
-});
-
-
-/*
-$(document).ready( function(){
-	var amform = $('#addMateForm');
-	amform.validate();
-});
-*/
-/*
-var parseAddMateForm = function(data){
-	console.log(data)
-};
-
-
-$(document).bind('pageinit', function(){
-
-	var amform = $('#addMateForm'),
-		amformErrorsLink = $('#amformErrorsLink')
-	;
-	
-	amform.validate({
-		invalidHandler: function(form, validator){
-			amformErrorsLink.click();
-		},
-		submitHandler: function(){
-			var data = amform.serializeArray();
-			parseAddMateForm(data);
-		} 
-=======
 			
 			var data = mateData.serializeArray();
 			parseMateData(data);
@@ -75,13 +29,8 @@ $(document).bind('pageinit', function(){
 		}
 
 		
->>>>>>> origin/gh-pages
 	});
 
 });
 
-<<<<<<< HEAD
-*/
-=======
 
->>>>>>> origin/gh-pages
