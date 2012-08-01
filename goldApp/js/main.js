db66ed09ed0932f5fdef6a770c277498e072dcdd
DaveyDaVinci <DaveyDaVinci@gmail.com>
2012-08-01 16:08:08 -0400

    Updated with not working save data

--- goldApp/js/main.js
+++ goldApp/js/main.js
@@ -4,41 +4,52 @@
 David Magee
 MiU 0612
 Project 3
+
 */
-//test
-$(document).ready(function(){
-	var parseMateData = function(data){
-		console.log(data);	
-	};
 
 
-	$('#addMateForm').on('pageinit', function(){
+var clear = function () {
+	var ask = confirm("Clear all saved data?");
+	if (localStorage === 0){
+		alert("There is no data to clear.");
+	}
+	else if(ask) {
+	localStorage.clear();
+		alert("All profiles have been deleted.");
+		location.reload();
+	return false;
+	}
 
-		var getGender = function (){
+$('#addAMate').on('pageinit', function () {
+
+	//Grabs my gender radio button value
+	var getGender = function (){
 				return $('input:radio[name=gender]:checked').val();
 		};
-		
-	
-		function saveData(key){
-			$('#addMateForm').validate({
+
+    
+
+	//Stores data to ls
+	var saveData = function (key) {
+		//jQuery Validation
+		$('#addMateForm').validate({
 			invalidHandler: function (form, validator) {},
 			submitHandler: function () {
 			$('#addMateForm').serializeArray();
 			}
 		});
-			if($('#addMateForm').valid()){
-		
-		
-		//If there is no key, its' a brand new item and we create a random key
-				if(!key){
-					var id 				= Math.floor(Math.random()*10000001);
-				}else{
-			//Sets the id to existing key to override data	
-					id = key;
-				}
-		//this retrieves and gathers our form values and store in object.
-		//Object properties contain array with the form label and input values.
-				var item				= {};
+		if ($('#addMateForm').valid()){ 	
+			var id;
+			// if there is no key --- this means this is a new item and needs a new key
+			if (!key) {
+				id = 				Math.floor(Math.random()*10000001);
+			}
+			// sets the id of existing key that is being edited so that it is saved over
+			else{
+				id = key;
+			}			
+			// gather form values and stores them
+			var item				= {};
 				item.planet				= ["Home Planet: ", $('homeplanets').val()];
 				item.skill				= ["Skill: ", $('theskills').val()];
 				item.name				= ["Name: ", $('name').val()];
@@ -47,25 +58,33 @@ $(document).ready(function(){
 				item.character			= ["Character: ", $('character').val()];
 				item.bio				= ["Bio: ", $('bio').val()];
 				item.gender				= ["Gender: ", getGender()];
-		//Save data into local storage: use Stringify to convert our object to a string.
-				localStorage.setItem(id, JSON.stringify(item));
-				alert("Profile Saved!");
-				
-			};
-			
-		$('#submit').on('click',function () {
-			saveData();
-		});
-	
+
+			// save data into localStorage
+			localStorage.setItem(id, JSON.stringify(item));
+			alert("Profile Saved!");
+			// reload page after confirming that the maintenance was saved
+			location.reload();
+		}
 	};
-		
+	// ----------------------------------------
 
-		
-
+	// Button event listeners --- display, clear, submit, and reset
+	//Clear button event listener
+	$('#clearData').on('click',function () {
+		clear();
 	});
+	// Submit button event listener
+	$('#submit').on('click',function () {
+		saveData();
+	});
+	// Reloads the form page so that everything is set back to default values
+	$('#reset').on('click',function () {
+		//reload page
+		location.reload();
+	});
+});	
 
 
 
-});
 
 
