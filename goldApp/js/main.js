d2ee3ac01367f0f5aed65ce27f5a80f750b3d9b7
DaveyDaVinci <DaveyDaVinci@gmail.com>
2012-08-01 20:34:41 -0400

    Updated again

--- goldApp/js/main.js
+++ goldApp/js/main.js
@@ -23,7 +23,7 @@ var clear = function () {
 	}
 };
 
-$(document).bind('pageinit', function(){
+$('#addAMate').on('pageinit', function(){
 
 	var mateData = $('#addMateForm');
 
@@ -40,6 +40,35 @@ $(document).bind('pageinit', function(){
 
 		
 	});
+	
+	var saveData = function (key) {
+		var id;
+			// if there is no key --- this means this is a new item and needs a new key
+			if (!key) {
+				id = 				Math.floor(Math.random()*10000001);
+			}
+			// sets the id of existing key that is being edited so that it is saved over
+			else{
+				id = key;
+			}			
+			// gather form values and stores them
+			var item				= {};
+				item.planet				= ["Home Planet: ", $('homeplanets').val()];
+				item.skill				= ["Skill: ", $('theskills').val()];
+				item.name				= ["Name: ", $('name').val()];
+				item.born				= ["Born: ", $('born').val()];
+				item.morality			= ["Morality: ", $('morality').val()];
+				item.character			= ["Character: ", $('character').val()];
+				item.bio				= ["Bio: ", $('bio').val()];
+				item.gender				= ["Gender: ", getGender()];
+
+			// save data into localStorage
+			localStorage.setItem(id, JSON.stringify(item));
+			alert("Profile Saved!");
+			// reload page after confirming that the profile was saved
+			location.reload();
+		}
+	};
 
 });
 
