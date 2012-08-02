614a931e2c6ca107f012a9893046deb91bd0d906
DaveyDaVinci <DaveyDaVinci@gmail.com>
2012-08-02 16:19:16 -0400

    NOthing is working.

--- goldApp/js/main.js
+++ goldApp/js/main.js
@@ -23,8 +23,9 @@ var clear = function () {
 	}
 };
 
-$('#addAMate').on('pageinit', function(){
+//Save mate page
 
+$('#addAMate').on('pageinit', function(){
 	var mateData = $('#addMateForm');
 
 	mateData.validate({
@@ -32,18 +33,15 @@ $('#addAMate').on('pageinit', function(){
 		submitHandler: function(){
 			
 			var data = mateData.serializeArray();
-			parseMateData(data);
-			localStorage.setItem('formdata', data);
-		
-			
+			saveData();
 		}
-
-		
 	});
+	var getGender = function (){
+				return $('input:radio[name=gender]:checked').val();
+		};
 	
-	/*
 	var saveData = function (key) {
-		var id;
+		
 			// if there is no key --- this means this is a new item and needs a new key
 			if (!key) {
 				id = 				Math.floor(Math.random()*10000001);
@@ -54,8 +52,8 @@ $('#addAMate').on('pageinit', function(){
 			}			
 			// gather form values and stores them
 			var item				= {};
-				item.planet				= ["Home Planet: ", $('homeplanets').val()];
-				item.skill				= ["Skill: ", $('theskills').val()];
+				item.planet				= ["Home Planet: ", $('homePlanet').val()];
+				item.skill				= ["Skill: ", $('skills').val()];
 				item.name				= ["Name: ", $('name').val()];
 				item.born				= ["Born: ", $('born').val()];
 				item.morality			= ["Morality: ", $('morality').val()];
@@ -68,10 +66,90 @@ $('#addAMate').on('pageinit', function(){
 			alert("Profile Saved!");
 			// reload page after confirming that the profile was saved
 			location.reload();
-		}
 	}
-*/
+
+
+
+
+function toggleControls(n){
+			switch(n){
+				case "on":
+					$('#addAMate').hide(); //NEED PROPER TAG
+					$('#clearData').css('display', 'inline'); = "inline"; //NEED PROPER TAG
+					$('#displayData').hide();
+					$('#newData').style.display = "inline";
+					break; 
+				case "off":
+					$('#addAMate').show();
+					$('#clearData').css('display', 'inline'); 
+					$('#displayData').css('display', 'inline');
+					$('#newData').hide();
+					$('#info').hide();
+					break;
+				default:
+					return false;
+			}
+	}
+
+
+	function getData(){
+		toggleControls('on');
+		if(localStorage.length === 0){
+			alert("Nothing is stored in local storage. Default profiles were constructed.");
+			constructDefaults();
+		}
+		var createDiv = $('div');
+		$('#createDiv').attr("id", "info");
+		var makeList = $('ul');
+		$('createDiv').append('makeList');
+		$('document.body').append('createDiv');
+		$('info').show();
+		for(i=0, j=localStorage.length; i<j; i++){
+			var makeli = $('li');
+			var linksLi = $('li');
+			$('makeList').append('makeli');
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var object = JSON.parse(value); //This converts string data back to object
+			var sublist = $('ul');
+			$('makeli').append('sublist');
+			getImage(object.planet[1], sublist); 
+			for(var n in object){
+				var listItems = $('li');
+				$('sublist').append('listItems');
+				var subText = object[n][0] +" "+object[n][1];
+				listItems.innerHTML = subText;
+				$('listItems').append('linksLi');
+			}
+			createItemLinks(localStorage.key(i), linksLi); //This creates the buttons for each item in the storage.
+		}
+	};
+
+	function getImage(planName, sublist){
+		var imageLi = $('li');
+		$('sublist').append('imageLi');
+		var imageTag = $('img');
+		var setSource = $('#imageTag').attr("src", "img/" + planName + ".png");
+		$('imageLi').append('imageTag');
+	
+	};
+	
+	
+	
+	
+	
+	
+	function constructDefaults(){
+		//Store the JSON data into local storage so we have default data
+		for(var n in json){
+			var id 				= Math.floor(Math.random()*10000001);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+		}
+	
+	}
+
 });
+
 
 
 /*
@@ -118,7 +196,7 @@ $('#addAMate').on('pageinit', function () {
 			// save data into localStorage
 			localStorage.setItem(id, JSON.stringify(item));
 			alert("Profile Saved!");
-			// reload page after confirming that the maintenance was saved
+			// reload page after confirming that the maintenan$ was saved
 			location.reload();
 		}
 	};
